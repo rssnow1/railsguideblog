40283902ad848f9acb743d36a40755980d8418a8
Richard Snow <richard@richard-M68MT-D3.(none)>
2011-12-13 13:44:25 -0600

    Finish Chapter 7

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ActiveRecord::Base
   validates :name,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 }
+	has_many :comments
 end
