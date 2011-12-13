ffb6d321f07154eaf9f981b6e5c5069a128b6c6d
root <root@richard-M68MT-D3.(none)>
2011-12-13 14:17:47 -0600

    chapter 11

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111213193213) do
+ActiveRecord::Schema.define(:version => 20111213201224) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -29,5 +29,14 @@ ActiveRecord::Schema.define(:version => 20111213193213) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "tags", ["post_id"], :name => "index_tags_on_post_id"
 
 end
